350fb3d7b8fe0f20627e06b9fd83a538ed1fc417
David Busch <buschdr@gmail.com>
2022-03-28 10:58:20 -0500

    Fib

--- RecursionExample_SP22/Main.cpp
+++ RecursionExample_SP22/Main.cpp
@@ -4,10 +4,11 @@
 using namespace std;
 
 int Factorial(int n);
+int fib(int n);
 
 int main()
 {
-	int value = 50;
+/*	int value = 50;
 	for (int j = 0; j < 10; j++)
 	{
 		chrono::time_point<chrono::system_clock> start;
@@ -28,6 +29,35 @@ int main()
 		end = chrono::system_clock::now();
 		elapsed = end - start;
 		cout << "Loop time: " << elapsed.count() << endl;
+	}*/
+
+	int index = 45;
+	for (int j = 0; j < 1; j++)
+	{
+		chrono::time_point<chrono::system_clock> start;
+		chrono::time_point<chrono::system_clock> end;
+		chrono::duration<float> elapsed;
+
+		start = chrono::system_clock::now();
+		int rec = fib(index);
+		end = chrono::system_clock::now();
+		elapsed = end - start;
+		cout << "Rec: " << rec << " ";
+		cout << "Recursive time: " << elapsed.count() << endl;
+
+		int a = 1, b = 1, value;
+
+		start = chrono::system_clock::now();
+		for (int i = 2; i <= index; i++)
+		{
+			value = a + b;
+			a = b;
+			b = value;
+		}
+		end = chrono::system_clock::now();
+		elapsed = end - start;
+		cout << "Loop: " << value << " ";
+		cout << "Loop time: " << elapsed.count() << endl;
 	}
 }
 
@@ -37,4 +67,14 @@ int Factorial(int n)
 		return 1;
 
 	return n * Factorial(n - 1);
+}
+
+int fib(int n)
+{
+	if (n == 0 || n == 1)
+		return 1;
+
+	cout << "Fib(" << n << ")";
+
+	return fib(n - 1) + fib(n - 2);
 }
